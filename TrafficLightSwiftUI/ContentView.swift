cb574430a20053cbebed72c5b14d7c1d2d6f109d
meowfield <meowfield@yandex.ru>
2024-10-09 20:13:23 +0200

    working traffic light

--- TrafficLightSwiftUI/ContentView.swift
+++ TrafficLightSwiftUI/ContentView.swift
@@ -8,14 +8,60 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    enum TrafficLight {
+        case red, yellow, green
+    }
+    
+    private let lightIsOffState = 0.3
+    private let lightIsOnState = 1.0
+    @State private var currentLight: TrafficLight = .red
+    @State private var buttonName = "Start"
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Circle()
+                .fill(Color.red)
+                .opacity(currentLight == .red ? lightIsOnState : lightIsOffState)
+                .frame(width: 100, height: 100)
+                .overlay(Circle().stroke(Color.white, lineWidth: 2))
+            Circle()
+                .fill(Color.yellow)
+                .opacity(currentLight == .yellow ? lightIsOnState : lightIsOffState)
+                .frame(width: 100, height: 100)
+                .overlay(Circle().stroke(Color.white, lineWidth: 2))
+            Circle()
+                .fill(Color.green)
+                .opacity(currentLight == .green ? lightIsOnState : lightIsOffState)
+                .frame(width: 100, height: 100)
+                .overlay(Circle().stroke(Color.white, lineWidth: 2))
+        }
+        .padding(.bottom, 350)
+        
+        VStack {
+            Button(buttonName) {
+                changeTrafficLightState()
+                if buttonName == "Start" {
+                        buttonName = "Next"
+                    }
+            }
         }
         .padding()
+        .frame(width: 150, height: 50)
+        .background(RoundedRectangle(cornerRadius: 40)
+            .fill(Color.blue))
+        .foregroundStyle(Color.white)
+    }
+    
+    func changeTrafficLightState() {
+        switch currentLight {
+        case .red:
+            currentLight = .yellow
+        case .yellow:
+            currentLight = .green
+        case .green:
+            currentLight = .red
+        }
     }
 }
 
